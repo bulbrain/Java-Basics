47a90006cd66fd90da770f67bd24c2e4757691ca
bulbrain <georgi_d_g1@abv.bg>
2014-09-20 19:14:31 +0200

    added intro java homework

---
+++ 1. Introduction-to-Java-Homework/_05_CurrentDateTime.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+// Create a simple Java program CurrentDateTime.java to print the current date and
+// time. Submit the Java class CurrentDateTime as part of your homework.
+public class _05_CurrentDateTime {
+public static void main(String[] args) {
+	System.out.println(LocalDateTime.now());
+}
+}
---
+++ 1. Introduction-to-Java-Homework/_06_SumTwoNumbers.java
@@ -0,0 +1,12 @@
+// Write a program SumTwoNumbers.java that enters two integers from the console, 
+// calculates and prints their sum. Search in Internet to learn how to read numbers 
+// from the console.
+import java.util.Scanner;
+public class _06_SumTwoNumbers {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int a = in.nextInt();
+	int b = in.nextInt();
+	System.out.println(a + b);
+}
+}
